33ad0918f7652af16aeaa7b9287540f04e731b65
ivandronovais <ivandronovais@gmail.com>
2025-03-15 13:14:53 -0300

    ✨ feat(cliente): add Endereco composition to Cliente class

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,18 @@
+import java.util.Date;
 
 public class Cliente {
 
 	private String nome;
+	private String cpf;
+	private Endereco endereco;
+	private Date dataNascimento;
+	
+	public Cliente(String nome, String cpf, Endereco endereco, Date dataNascimento) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.dataNascimento = dataNascimento;
+	}
 
 	public String getNome() {
 		return nome;
@@ -11,4 +22,28 @@ public class Cliente {
 		this.nome = nome;
 	}
 
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
 }
